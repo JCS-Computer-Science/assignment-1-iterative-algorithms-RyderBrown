013847e80437a2d60b9bc6dca5a9a291803079ee
Ryder Brown <1100054954@edu.cbe.ab.ca>
2024-02-12 15:20:17 -0700

    Finished Bubble Sorting

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,5 +1,20 @@
 function bubbleSort(array) {
-	/*
+	
+    for (let j = 0; j < array.length; j++) {
+        for (let i = 0; i < array.length; i++) {
+        
+            if (array[i] > array[i + 1]) {
+                let temp1 = array[i]
+                let temp2 = array[i + 1]
+                array[i] = temp2
+                array[i + 1] = temp1
+            }
+        }
+        
+    }
+
+    
+    /*
     ** Compare the first two elements and swap if necessary.
     ** Then compare the second and third elements and swap if
     ** necessary. Continue until you've checked and swapped (if needed)
