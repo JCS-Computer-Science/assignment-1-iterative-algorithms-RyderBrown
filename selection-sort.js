154e3fb7d7610a0adf0301d905bf8d1dd0b42571
Ryder Brown <1100054954@edu.cbe.ab.ca>
2024-02-20 13:57:54 -0700

    Starting Selection Sort

--- selection-sort.js
+++ selection-sort.js
@@ -1,4 +1,20 @@
 function selectionSort(array) {
+		
+	for (let i = 0; i < array.length; i++) {
+		let lowest = i
+
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] < array[lowest]) {
+				lowest = j
+			}
+		}
+
+		if (lowest !== i) {
+			[array[i], array[lowest]] = [array[lowest], array[i]]
+		}
+		return array;
+	}
+	
 	/*
 	 ** Search through the whole array for the smallest element
 	 ** then swap it with the first element.
