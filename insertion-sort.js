0fa5150d71a98be3f5be4afb2691aa2dbac6a1b0
Ryder Brown <1100054954@edu.cbe.ab.ca>
2024-02-20 13:42:29 -0700

    Finished Insertion Sort

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,4 +1,16 @@
 function insertionSort(array) {
+	
+	let j;
+	
+	for (let i = 0; i < array.length; i++) {
+		let arrayValue = array[i]
+		for (j = i -1; j >= 0 && array[j] > arrayValue; j--) {
+			array[j + 1] = array[j]
+		}
+		array[j + 1] = arrayValue
+	}
+	return array;
+
 	/*
 	 ** For each element in the array, swap it with the element
 	 ** before it over and over as long as the element before it
